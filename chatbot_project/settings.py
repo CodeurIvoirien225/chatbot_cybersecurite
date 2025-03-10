3f007751b405f291a7e79584307ed97d5f3b04b9
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-03-10 23:53:51 +0000

    Correction des fichiers statiques

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = '/static/'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Répertoire où Render stocke les fichiers statiques
 
-STATIC_URL = '/static/'  # URL pour servir les fichiers statiques
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Dossier contenant les fichiers statiques
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Dossier de collecte des fichiers
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chatbot', 'static'),  # Dossier contenant tes fichiers CSS et JS
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
