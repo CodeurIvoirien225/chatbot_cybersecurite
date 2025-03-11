a9eee15d5f48e5f2883fee706b0a9db0296c84c7
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-03-11 14:25:33 +0000

    Mise à jour du fichier

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatbot',
-    'corsheaders',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
